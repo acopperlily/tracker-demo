47ea47cdc7687f77c504809da215be6b11544316
acopperlily <acopperlily@yahoo.com>
2022-10-28 10:14:35 -0400

    Add Tasks file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,31 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click');
+  };
+  
   return (
-    <header>
-      <h1>Task Tracker</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
-}
+};
+
+Header.defaultProps = {
+  title: 'Task Tracker'
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+
+// You can style stuff inline with variables if you want to
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// };
 
 export default Header
