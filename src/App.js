a7be9e42833a25fb110eb39da932586261e1b678
acopperlily <acopperlily@yahoo.com>
2022-10-29 04:20:29 -0400

    Add files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'test',
+      other: 'no-show',
+      day: 'monday',
+      reminder: true,
+    },
+    {
+      text: 'Test4',
+      day: 'Wednesday',
+      reminder: false,
+      id: 3,
+    },
+    {
+      text: 'Task6',
+      day: 'Thursday',
+      reminder: true,
+      id: 4,
+    },
+  ]);
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder } : task));
+  };
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      {tasks.length > 0
+        ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+        : 'No tasks to display'}
     </div>
   );
 };
